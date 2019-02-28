49e933c05e51539715c068d7f22eab183c1a5028
vikrant-phadtare1101 <vikrant.phadtare@acellere.com>
2019-02-28 10:42:53 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+package com.mariten.kanatools;
+import com.mariten.kanatools.KanaAppraiser;
+
+public abstract class KanaAppraiserTester
+{
+    public KanaAppraiserTester()
+    {
+        // Test instantiation
+        KanaAppraiser kana_evaluator_object = new KanaAppraiser();
+    }
+}
